41ca759237549f4efbaa250924fcc07d08f53dfe
skyaree <vphonegaga9001@gmail.com>
2026-06-11 02:19:36 +0500

    test4

--- app/src/main/java/com/squeeare/hidelockwidgets/ui/SettingsScreen.kt
+++ app/src/main/java/com/squeeare/hidelockwidgets/ui/SettingsScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
